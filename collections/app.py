ce727239a6cb38d85b9785e0bb080ed5abd187e8
Bo Lu <bo.lu@canada.ca>
2022-12-22 15:24:08 -0500

    Fixes an issue where record that have both parent and children was not displaying correctly

--- collections/app.py
+++ collections/app.py
@@ -34,13 +34,21 @@ def lambda_handler(event, context):
                 'statusCode': 200,
                 'body': json.dumps(message)
             }
-        
+        #self
         self_json = find_self(geocore_df, uuid)
         
+        #parent
         parent_json, parent_id  = find_parent(geocore_df, uuid)
         
-        child_json, child_count  = find_children(geocore_df, parent_id, uuid)
-
+        #sibling
+        sibling_json = None
+        sibling_count = 0
+        if parent_json != None:
+            sibling_json, sibling_count  = find_siblings(geocore_df, parent_id, uuid)
+            
+        #child
+        child_json, child_count = find_children(geocore_df, uuid)
+        
     else:
         message += "No id parameter was passed. Usage: ?id=XYZ"
         return {
@@ -50,10 +58,12 @@ def lambda_handler(event, context):
     
     return {
         'statusCode': 200,
-        'sibling_count': child_count,
+        'sibling_count': sibling_count,
+        'child_count': child_count,
         'self': self_json,
         'parent': parent_json,
-        'sibling': child_json
+        'sibling': sibling_json,
+        'child': child_json
     }
 
 def find_self(geocore_df, uuid):
@@ -107,7 +117,7 @@ def find_parent(geocore_df, uuid):
 
     return nonesafe_loads(parent_message), parent_id
 
-def find_children(geocore_df, parent_id, uuid):
+def find_siblings(geocore_df, parent_id, uuid):
     """
     Find sibling records of a uuid if it exists
     :param geocore_df: dataframe containing all geocore records
@@ -119,6 +129,39 @@ def find_children(geocore_df, parent_id, uuid):
     child_array_desc_en = []
     child_array_desc_fr = []
     other_children_df = geocore_df[geocore_df['features_properties_parentIdentifier'] == parent_id]
+    
+    #cannot be its own sibling. remove self from the siblings dataframe
+    other_children_df = other_children_df[other_children_df['features_properties_id'] != uuid]
+    
+    if len(other_children_df) == 0:
+        child_message = None
+    else:
+        child_message = "["
+        for i in range(0,len(other_children_df)):
+            child_array_id.append(other_children_df.iloc[i]['features_properties_id'])
+            child_array_desc_en.append(other_children_df.iloc[i]['features_properties_title_en'])
+            child_array_desc_fr.append(other_children_df.iloc[i]['features_properties_title_fr'])
+
+        for i in range(0,len(other_children_df)):
+            child_message += '{ "id": "' + child_array_id[i] + '", "description_en": "' + child_array_desc_en[i] + '", "description_fr": "' + child_array_desc_fr[i] + '"}'
+            if i != len(other_children_df)-1:
+                child_message += ', '
+        child_message += "]"
+    
+    return nonesafe_loads(child_message), len(other_children_df)
+    
+def find_children(geocore_df, uuid):
+    """
+    Find child records if it exists
+    :param geocore_df: dataframe containing all geocore records
+    :param uuid: unique id we are looking up
+    :return child_json: JSON of the child uuid and record title in english and french
+    :return child_count: count of child records
+    """
+    child_array_id = []
+    child_array_desc_en = []
+    child_array_desc_fr = []
+    other_children_df = geocore_df[geocore_df['features_properties_parentIdentifier'] == uuid]
     
     #cannot be its own sibling. remove self from the siblings dataframe
     other_children_df = other_children_df[other_children_df['features_properties_id'] != uuid]
